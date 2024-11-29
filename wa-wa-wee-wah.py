36f7e814e7446239bce250eff5a59fc1a1db8c77
Howie Benefiel <5847863+howinator@users.noreply.github.com>
2024-11-28 19:13:36 -0600

    Parker's first computer program

---
+++ wa-wa-wee-wah.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+from playsound import playsound
+while True:
+  print("wa wa wee wah")
+  print("Parker is thankful for his family and friends and cousins")
+  print("Parker is thankful for his house.")
+  print("Parker likes to bounce on the bounce house and slide")
+  print("Parker likes maccaroni")
+  print("Parker likes to bike with his family")
+  print("PARKER LOVES HOWIE")
+  print("Parker can't wait to go mountain biking with Emily and Howie")
+  playsound("/Users/howie/Downloads/wa2.m4a")	
+  sleep(0.5)
